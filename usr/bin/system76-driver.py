03a8123850b082af65a62e5b3dfd7275fa992268
Ian Santopietro <ian@system76.com>
2012-05-25 11:50:08 -0600

    Replaced forgotten startup script with version that checks for GTK3 or later.

--- usr/bin/system76-driver.py
+++ usr/bin/system76-driver.py
@@ -7,6 +7,9 @@
 ## Released under the GNU General Public License (See LICENSE)
 ##
 ## Startup script
+#
+#
+##TODO: Remove debugging OS version string for final release!
 
 import os
 import sys
@@ -19,15 +22,17 @@ if os.path.isdir(dir):
 import base_system
 import driverscontrol
 import optparse
-import System76Driver
+import ubuntuversion
 
+osversion = ubuntuversion.release()
+driverversion = ubuntuversion.driver()
 def main():
     
-    parser = optparse.OptionParser(usage="%prog [options]", version="2.7.6")
+    parser = optparse.OptionParser(usage="%prog [options]", version=driverversion)
     parser.add_option("-d", "--drivers", action="store_true", dest="drivers",
-				help="Install Drivers for your Computer (requires sudo)")
+                help="Install Drivers for your Computer (requires sudo)")
     parser.add_option("-r", "--restore", action="store_true", dest="restore",
-				help="Restore Computer to Factory Defaults (requires sudo)")
+                help="Restore Computer to Factory Defaults (requires sudo)")
 
     (options, args) = parser.parse_args()
 
@@ -37,14 +42,32 @@ def main():
     elif options.drivers:
         driverscontrol.installDrivers()
     else:
-        try:
-            os.environ['KDE_FULL_SESSION'] == 'true'
-            os.system('kdesudo --comment "System76 Driver" python /opt/system76/system76-driver/src/System76Driver.py')
-        except:
-            os.environ['GDMSESSION'] == 'default'
-            os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Driver.py')
+    
+        if osversion != '6.06' and osversion != '6.10' and osversion != '7.04' and osversion != '7.10' and osversion != '8.04' and osversion != '8.10' and osversion != '9.04' and osversion != '9.10' and osversion != '10.04' and osversion != '10.10' and osversion != '11.04':
+            try:
+                print("NOTE: 11.10 or later detected! Running GTK3 version.")
+                os.environ['KDE_FULL_SESSION'] == 'true'
+                os.system('kdesudo --comment "System76 Driver" python /opt/system76/system76-driver/src/System76Drivergtk3.py')
+            except:
+                print("NOTE: 11.10 or later detected! Running GTK3 version.")
+                os.environ['GDMSESSION'] == 'default'
+                os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Drivergtk3.py')
+            else:
+                print("NOTE: 11.10 or later detected! Running GTK3 version.")
+                os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Drivergtk3.py')
         else:
-            os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Driver.py')
+            try:
+                print("NOTE: 11.04 or earlier detected! Running GTK2 version.")
+                os.environ['KDE_FULL_SESSION'] == 'true'
+                os.system('kdesudo --comment "System76 Driver" python /opt/system76/system76-driver/src/System76Driver.py')
+            except:
+                print("NOTE: 11.04 or earlier detected! Running GTK2 version.")
+                os.environ['GDMSESSION'] == 'default'
+                os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Driver.py')
+            else:
+                print("NOTE: 11.04 or earlier detected! Running GTK2 version.")
+                os.system('gksu --description /usr/share/applications/system76-driver.desktop python /opt/system76/system76-driver/src/System76Driver.py')
 
 if __name__ == "__main__":
     main()
+
