aa9df48bd2100bbefb06051c02268c0b450f2e8c
Jason Gerard DeRose <jason@system76.com>
2013-05-28 18:33:02 -0600

    Added determine_model() and its test

--- system76driver/model.py
+++ system76driver/model.py
@@ -31,8 +31,104 @@ KEYWORDS = (
     'system-version',
 )
 
-SYSTEM_UUID = {
-    '00000000-0000-0000-0000-000000000001': 'koap1',
+TABLES = {
+    'system-uuid': {
+        '00000000-0000-0000-0000-000000000001': 'koap1',
+    },
+    'baseboard-product-name': {
+        'Z35FM': 'daru1',
+        'Z35F': 'daru1',
+        'MS-1221': 'daru2',
+        'IFL91': 'panv3',
+        'IFT01': 'gazv5',
+        'IFT00': 'gazp5',
+        'A8N8L': 'sabv1',
+        'M2N8L': 'sabv2',
+        'P5K-VM': 'sabv3',
+        'IFL90': 'serp3',
+        'JFL92': 'serp4',
+        'MS-7250': 'wilp1',
+        'A8V-MQ': 'ratv1',
+        'P5VD2-MX': 'ratv2',
+        'P5VD2-VM': 'ratv3',
+        'D945GCPE': 'ratv4',
+        'P5GC-MX/1333': 'ratv5',
+        'MPAD-MSAE Customer Reference Boards': 'gazv2',
+        'K8N-DL': 'wilp2',
+        'KFN5-D SLI': 'wilp3',
+        'DP35DP': 'wilp5',
+    },
+    'system-product-name': {
+        'MS-1012': 'gazv1',
+        'Z62FP': 'gazv3',
+        'Z62FM': 'gazv4',
+        'Z62F': 'gazp1',
+        'Z62J': 'gazp2',
+        'Z62JM': 'gazp3',
+        'Z62JP': 'gazp3',
+        'U-100': 'meec1',
+        'Z96F': 'panv2',
+        'Centoris V661': 'panv2',
+        'Z96FM': 'panv2',
+        'HEL80I': 'serp1',
+        'HEL8X': 'serp1',
+        'HEL80C': 'serp2',
+        'UW1': 'star1',
+        'star1': 'star1',
+        'E10IS': 'star2',
+        'E10IS2': 'star2',
+        'Star2': 'star2',
+        'A7V': 'bonp1',
+        'M570TU': 'bonp2',
+        'M720T/M730T': 'daru3',
+        'M740T/M760T': 'panp4i',
+        'M740TU/M760TU': 'panp4n',
+        'M860TU': 'serp5',
+    },
+    'system-version': {
+        'bonp2': 'bonp2',
+        'bonp3': 'bonp3',
+        'bonp4': 'bonp4',
+        'bonp5': 'bonp5',
+        'gazu1': 'gazu1',
+        'gazp6': 'gazp6',
+        'gazp7': 'gazp7',
+        'gazp8': 'gazp8',
+        'daru3': 'daru3',
+        'panp4n': 'panp4n',
+        'panp5': 'panp5',
+        'panp6': 'panp6',
+        'panp7': 'panp7',
+        'panp8': 'panp8',
+        'panp9': 'panp9',
+        'lemu1': 'lemu1',
+        'lemu2': 'lemu2',
+        'lemu3': 'lemu3',
+        'lemu4': 'lemu4',
+        'leo1': 'leo1',
+        'leox2': 'leox2',
+        'leox3': 'leox3',
+        'ment1': 'ment1',
+        'ment2': 'ment2',
+        'ment3': 'ment3',
+        'ment5': 'ment5',
+        'ratv6': 'ratv6',
+        'ratu1': 'ratu1',
+        'ratu2': 'ratu2',
+        'ratp1': 'ratp1',
+        'star3': 'star3',
+        'star4': 'star4',
+        'star5': 'star5',
+        'wilb1': 'wilb1',
+        'wilb2': 'wilb2',
+        'wilp6': 'wilp6',
+        'wilp7': 'wilp7',
+        'wilp8': 'wilp8',
+        'wilp9': 'wilp9',
+        'serp5': 'serp5',
+        'serp6': 'serp6',
+        'serp7': 'serp7',
+    },
 }
 
 
@@ -53,6 +149,11 @@ def determine_model(info=None):
     """
     if info is None:
         info = get_dmi_info()
-    
-    # Determine system unique value    
-    b = os.popen('sudo dmidecode -s system-uuid')
+
+    for keyword in KEYWORDS:
+        value = info[keyword]
+        table = TABLES[keyword]
+        if value in table:
+            return table[value]
+
+    return 'nonsystem76'
--- system76driver/tests/test_model.py
+++ system76driver/tests/test_model.py
@@ -42,6 +42,29 @@ EXPECTED = (
 )
 
 
+class TestConstants(TestCase):
+    def test_KEYWORDS(self):
+        self.assertIsInstance(model.KEYWORDS, tuple)
+        self.assertEqual(len(model.KEYWORDS), 4)
+        for keyword in model.KEYWORDS:
+            self.assertIsInstance(keyword, str)
+
+    def test_TABLES(self):
+        self.assertIsInstance(model.TABLES, dict)
+        self.assertEqual(len(model.TABLES), 4)
+        self.assertEqual(set(model.TABLES), set(model.KEYWORDS))
+        for (key, value) in model.TABLES.items():
+            self.assertIsInstance(key, str)
+            self.assertIsInstance(value, dict)
+            self.assertGreater(len(value), 0)
+            for (k, v) in value.items():
+                self.assertIsInstance(k, str)
+                self.assertIsInstance(v, str)
+        # 'system-version' is currently always the same:
+        for (k, v) in model.TABLES['system-version'].items():
+            self.assertEqual(k, v)
+
+
 class TestFunctions(TestCase):
     def test_dmidecode(self):
         SubProcess.reset(True, [b'bar\n'])
@@ -60,5 +83,87 @@ class TestFunctions(TestCase):
             ('check_output', ['sudo', 'dmidecode', '-s', 'system-product-name'], {}),
             ('check_output', ['sudo', 'dmidecode', '-s', 'system-version'], {}),
         ])
+        self.assertEqual(SubProcess.outputs, [])
+
+    def test_determine_model_1(self):
+        """
+        Test `determine_model()` when *info* is provided.
+        """
+        SubProcess.reset(True, OUTPUTS)
+        self.assertEqual(model.determine_model(), 'gazp7')
+        self.assertEqual(SubProcess.calls, [
+            ('check_output', ['sudo', 'dmidecode', '-s', 'system-uuid'], {}),
+            ('check_output', ['sudo', 'dmidecode', '-s', 'baseboard-product-name'], {}),
+            ('check_output', ['sudo', 'dmidecode', '-s', 'system-product-name'], {}),
+            ('check_output', ['sudo', 'dmidecode', '-s', 'system-version'], {}),
+        ])
+        self.assertEqual(SubProcess.outputs, [])
+
+    def test_determine_model_2(self):
+        """
+        Test `determine_model()` when *info* is provided.
+        """
+        SubProcess.reset(True)
+
+        # system-uuid:
+        info = {'system-uuid': '00000000-0000-0000-0000-000000000001'}
+        self.assertEqual(model.determine_model(info), 'koap1')
+
+        # baseboard-product-name:
+        info = {'system-uuid': 'nope', 'baseboard-product-name': 'Z35FM'}
+        self.assertEqual(model.determine_model(info), 'daru1')
+
+        info = {'system-uuid': 'nope', 'baseboard-product-name': 'Z35F'}
+        self.assertEqual(model.determine_model(info), 'daru1')
+
+        info = {'system-uuid': 'nope', 'baseboard-product-name': 'MS-1221'}
+        self.assertEqual(model.determine_model(info), 'daru2')
+
+        info = {'system-uuid': 'nope', 'baseboard-product-name': 'MS-1221'}
+        self.assertEqual(model.determine_model(info), 'daru2')
+
+        # system-uuid:
+        for (key, value) in model.TABLES['system-uuid'].items():
+            info = {'system-uuid': key}
+            self.assertEqual(model.determine_model(info), value)
+
+        # baseboard-product-name:
+        for (key, value) in model.TABLES['baseboard-product-name'].items():
+            info = {
+                'system-uuid': 'nope',
+                'baseboard-product-name': key,
+            }
+            self.assertEqual(model.determine_model(info), value)
+
+        # system-product-name:
+        for (key, value) in model.TABLES['system-product-name'].items():
+            info = {
+                'system-uuid': 'nope',
+                'baseboard-product-name': 'nope',
+                'system-product-name': key,
+            }
+            self.assertEqual(model.determine_model(info), value)
+
+        # system-version:
+        for (key, value) in model.TABLES['system-version'].items():
+            info = {
+                'system-uuid': 'nope',
+                'baseboard-product-name': 'nope',
+                'system-product-name': 'nope',
+                'system-version': key,
+            }
+            self.assertEqual(model.determine_model(info), value)
+
+        # non-System76:
+        info = {
+            'system-uuid': 'nope',
+            'baseboard-product-name': 'nope',
+            'system-product-name': 'nope',
+            'system-version': 'nope',
+        }
+        self.assertEqual(model.determine_model(info), 'nonsystem76')
+
+        # No calls should have resulted:
+        self.assertEqual(SubProcess.calls, [])
         self.assertEqual(SubProcess.outputs, [])
 
