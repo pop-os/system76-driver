5bae53c50e7ae4bd89a37aeb55f0697f0887608c
Jason Gerard DeRose <jason@system76.com>
2013-06-14 15:09:50 -0600

    Let's keep the PRODUCTS dict in pretty-print form

--- system76driver/products.py
+++ system76driver/products.py
@@ -18,7 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 """
-Map product name to list of driver actions.
+Map model name to list of driver actions.
 """
 
 from . import actions
@@ -26,123 +26,438 @@ from . import actions
 
 PRODUCTS = {
     # Bonobo:
-    'bonp1': {'name': 'Bonobo Performance', 'drivers': [], 'prefs': []},
-    'bonp2': {'name': 'Bonobo Performance', 'drivers': [], 'prefs': []},
-    'bonp3': {'name': 'Bonobo Performance', 'drivers': [], 'prefs': []},
-    'bonp4': {'name': 'Bonobo Professional', 'drivers': [], 'prefs': []},
-    'bonp5': {'name': 'Bonobo Professional', 'drivers': [], 'prefs': []},
-    'bonx6': {'name': 'Bonobo Extreme',
+    'bonp1': {
+        'name': 'Bonobo Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'bonp2': {
+        'name': 'Bonobo Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'bonp3': {
+        'name': 'Bonobo Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'bonp4': {
+        'name': 'Bonobo Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'bonp5': {
+        'name': 'Bonobo Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'bonx6': {
+        'name': 'Bonobo Extreme',
         'drivers': [
             actions.fingerprintGUI,
             actions.plymouth1080,
             actions.wifi_pm_disable,
         ],
-        'prefs': []
+        'prefs': [],
     },
 
     # Darter:
-    'daru1': {'name': 'Darter Ultra', 'drivers': [], 'prefs': []},
-    'daru2': {'name': 'Darter Ultra', 'drivers': [], 'prefs': []},
-    'daru3': {'name': 'Darter Ultra', 'drivers': [], 'prefs': []},
-
-    # Gazelle:
-    'gazp1': {'name': 'Gazelle Performance', 'drivers': [], 'prefs': []},
-    'gazp2': {'name': 'Gazelle Performance', 'drivers': [], 'prefs': []},
-    'gazp3': {'name': 'Gazelle Performance', 'drivers': [], 'prefs': []},
-    'gazp5': {'name': 'Gazelle Value with nVidia and Camera',
+    'daru1': {
+        'name': 'Darter Ultra',
         'drivers': [],
         'prefs': [],
     },
-    'gazp6': {'name': 'Gazelle Professional', 'drivers': [], 'prefs': []},
-    'gazp7': {'name': 'Gazelle Professional', 'drivers': [], 'prefs': []},
-    'gazp8': {'name': 'Gazelle Professional', 'drivers': [], 'prefs': []},
-    'gazp9': {'name': 'Gazelle Professional', 'prefs': [],
-        'drivers': [actions.airplane_mode, actions.backlight_vendor],
+    'daru2': {
+        'name': 'Darter Ultra',
+        'drivers': [],
+        'prefs': [],
     },
-    'gazu1': {'name': 'Gazelle Ultra', 'drivers': [], 'prefs': []},
-    'gazv1': {'name': 'Gazelle Value', 'drivers': [], 'prefs': []},
-    'gazv2': {'name': 'Gazelle Value', 'drivers': [], 'prefs': []},
-    'gazv3': {'name': 'Gazelle Value', 'drivers': [], 'prefs': []},
-    'gazv4': {'name': 'Gazelle Value', 'drivers': [], 'prefs': []},
-    'gazv5': {'name': 'Gazelle Value', 'drivers': [], 'prefs': []},
+    'daru3': {
+        'name': 'Darter Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
+
+    # Gazelle:
+    'gazp1': {
+        'name': 'Gazelle Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp2': {
+        'name': 'Gazelle Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp3': {
+        'name': 'Gazelle Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp5': {
+        'name': 'Gazelle Value with nVidia and Camera',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp6': {
+        'name': 'Gazelle Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp7': {
+        'name': 'Gazelle Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp8': {
+        'name': 'Gazelle Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazp9': {
+        'name': 'Gazelle Professional',
+        'drivers': [
+            actions.airplane_mode,
+            actions.backlight_vendor,
+        ],
+        'prefs': [],
+    },
+    'gazu1': {
+        'name': 'Gazelle Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazv1': {
+        'name': 'Gazelle Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazv2': {
+        'name': 'Gazelle Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazv3': {
+        'name': 'Gazelle Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazv4': {
+        'name': 'Gazelle Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'gazv5': {
+        'name': 'Gazelle Value',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Koala:  
-    'koap1': {'name': 'Koala Performance', 'drivers': [], 'prefs': []},
+    'koap1': {
+        'name': 'Koala Performance',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Lemur:
-    'lemu1': {'name': 'Lemur UltraThin', 'drivers': [], 'prefs': []},
-    'lemu2': {'name': 'Lemur UltraThin', 'drivers': [], 'prefs': []},
-    'lemu3': {'name': 'Lemur Ultra', 'drivers': [], 'prefs': []},
-    'lemu4': {'name': 'Lemur Ultra', 'drivers': [], 'prefs': []},
+    'lemu1': {
+        'name': 'Lemur UltraThin',
+        'drivers': [],
+        'prefs': [],
+    },
+    'lemu2': {
+        'name': 'Lemur UltraThin',
+        'drivers': [],
+        'prefs': [],
+    },
+    'lemu3': {
+        'name': 'Lemur Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
+    'lemu4': {
+        'name': 'Lemur Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Leopard:
-    'leo1': {'name': 'The Leopard Extreme', 'drivers': [], 'prefs': []},
-    'leox2': {'name': 'The Leopard Extreme', 'drivers': [], 'prefs': []},
-    'leox3': {'name': 'The Leopard Extreme', 'drivers': [], 'prefs': []},
+    'leo1': {
+        'name': 'The Leopard Extreme',
+        'drivers': [],
+        'prefs': [],
+    },
+    'leox2': {
+        'name': 'The Leopard Extreme',
+        'drivers': [],
+        'prefs': [],
+    },
+    'leox3': {
+        'name': 'The Leopard Extreme',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Meerkat:
-    'meec1': {'name': 'Meerkat Compact', 'drivers': [], 'prefs': []},
-    'ment1': {'name': 'Meerkat NetTop', 'drivers': [], 'prefs': []},
-    'ment2': {'name': 'Meerkat Ion NetTop', 'drivers': [], 'prefs': []},
-    'ment3': {'name': 'Meerkat NetTop', 'drivers': [], 'prefs': []},
-    'ment5': {'name': 'Meerkat Ion NetTop', 'drivers': [], 'prefs': []},
+    'meec1': {
+        'name': 'Meerkat Compact',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ment1': {
+        'name': 'Meerkat NetTop',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ment2': {
+        'name': 'Meerkat Ion NetTop',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ment3': {
+        'name': 'Meerkat NetTop',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ment5': {
+        'name': 'Meerkat Ion NetTop',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Pangolin:
-    'panp4i': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp4n': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp5': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp6': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp7': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp8': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
-    'panp9': {'name': 'Pangolin Performance', 'drivers': [], 'prefs': []},
+    'panp4i': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp4n': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp5': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp6': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp7': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp8': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panp9': {
+        'name': 'Pangolin Performance',
+        'drivers': [],
+        'prefs': [],
+    },
     #'panv1': {'name': 'Pangolin Value'},  # FIXME: Not in model.py
-    'panv2': {'name': 'Pangolin Value', 'drivers': [], 'prefs': []},
-    'panv3': {'name': 'Pangolin Value', 'drivers': [], 'prefs': []},
+    'panv2': {
+        'name': 'Pangolin Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'panv3': {
+        'name': 'Pangolin Value',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Ratel:
-    'ratp1': {'name': 'Ratel Performance', 'drivers': [], 'prefs': []},
-    'ratu1': {'name': 'Ratel Ultra', 'drivers': [], 'prefs': []},
-    'ratu2': {'name': 'Ratel Ultra', 'drivers': [], 'prefs': []},
-    'ratv1': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
-    'ratv2': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
-    'ratv3': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
-    'ratv4': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
-    'ratv5': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
-    'ratv6': {'name': 'Ratel Value', 'drivers': [], 'prefs': []},
+    'ratp1': {
+        'name': 'Ratel Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratu1': {
+        'name': 'Ratel Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratu2': {
+        'name': 'Ratel Ultra',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv1': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv2': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv3': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv4': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv5': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
+    'ratv6': {
+        'name': 'Ratel Value',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Sable:
-    'sabc1': {'name': 'Sable Complete', 'drivers': [], 'prefs': []},
-    'sabv1': {'name': 'Sable Series', 'drivers': [], 'prefs': []},
-    'sabv2': {'name': 'Sable Series', 'drivers': [], 'prefs': []},
-    'sabv3': {'name': 'Sable Series', 'drivers': [], 'prefs': []},
+    'sabc1': {
+        'name': 'Sable Complete',
+        'drivers': [],
+        'prefs': [],
+    },
+    'sabv1': {
+        'name': 'Sable Series',
+        'drivers': [],
+        'prefs': [],
+    },
+    'sabv2': {
+        'name': 'Sable Series',
+        'drivers': [],
+        'prefs': [],
+    },
+    'sabv3': {
+        'name': 'Sable Series',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Serval:
-    'serp1': {'name': 'Serval Performance', 'drivers': [], 'prefs': []},
-    'serp2': {'name': 'Serval Performance', 'drivers': [], 'prefs': []},
-    'serp3': {'name': 'Serval Performance', 'drivers': [], 'prefs': []},
-    'serp4': {'name': 'Serval Performance', 'drivers': [], 'prefs': []},
-    'serp5': {'name': 'Serval Professional', 'drivers': [], 'prefs': []},
-    'serp6': {'name': 'Serval Professional', 'drivers': [], 'prefs': []},
-    'serp7': {'name': 'Serval Professional', 'drivers': [], 'prefs': []},
+    'serp1': {
+        'name': 'Serval Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp2': {
+        'name': 'Serval Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp3': {
+        'name': 'Serval Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp4': {
+        'name': 'Serval Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp5': {
+        'name': 'Serval Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp6': {
+        'name': 'Serval Professional',
+        'drivers': [],
+        'prefs': [],
+    },
+    'serp7': {
+        'name': 'Serval Professional',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Starling:
-    'star1': {'name': 'Starling NetBook', 'drivers': [], 'prefs': []},
-    'star2': {'name': 'Starling EduBook', 'drivers': [], 'prefs': []},
-    'star3': {'name': 'Starling NetBook', 'drivers': [], 'prefs': []},
-    'star4': {'name': 'Starling NetBook', 'drivers': [], 'prefs': []},
-    'star5': {'name': 'Starling NetBook', 'drivers': [], 'prefs': []},
+    'star1': {
+        'name': 'Starling NetBook',
+        'drivers': [],
+        'prefs': [],
+    },
+    'star2': {
+        'name': 'Starling EduBook',
+        'drivers': [],
+        'prefs': [],
+    },
+    'star3': {
+        'name': 'Starling NetBook',
+        'drivers': [],
+        'prefs': [],
+    },
+    'star4': {
+        'name': 'Starling NetBook',
+        'drivers': [],
+        'prefs': [],
+    },
+    'star5': {
+        'name': 'Starling NetBook',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Wildebeest:
-    'wilb1': {'name': 'Wildebeest Performance', 'drivers': [], 'prefs': []},
-    'wilb2': {'name': 'Wildebeest Performance', 'drivers': [], 'prefs': []},
+    'wilb1': {
+        'name': 'Wildebeest Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilb2': {
+        'name': 'Wildebeest Performance',
+        'drivers': [],
+        'prefs': [],
+    },
 
     # Wild Dog:
-    'wilp1': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp2': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp3': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp5': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp6': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp7': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp8': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
-    'wilp9': {'name': 'Wild Dog Performance', 'drivers': [], 'prefs': []},
+    'wilp1': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp2': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp3': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp5': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp6': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp7': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp8': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
+    'wilp9': {
+        'name': 'Wild Dog Performance',
+        'drivers': [],
+        'prefs': [],
+    },
 }
