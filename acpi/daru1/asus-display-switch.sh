afe94132a6bac038935f7e40498494abf3068f11
Jason Gerard DeRose <jason@system76.com>
2013-06-21 22:35:24 -0600

    rm crutft: acpi*, rtl8187b*, uvc*

--- acpi/daru1/asus-display-switch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo $1 | sed -re 's/^hotkey ATKD 0+6([1-3]) [0-9a-f]+$/\1/' > /proc/acpi/asus/disp
--- uvc/svn-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rev=`LANGUAGE=C svn info | grep Revision | cut -d ' ' -f 2`
-
-if [ x != x$rev ]; then
-	echo "#define DRIVER_VERSION	\"SVN r$rev\""
-fi
-
