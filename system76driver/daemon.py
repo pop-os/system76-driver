21beb5588e7a5f24b08a287812d3ab34a5a5959e
Jason Gerard DeRose <jason@system76.com>
2013-07-05 14:24:19 -0600

    Added 'galu1' to DEFAULT_BRIGHTNESS

--- system76driver/daemon.py
+++ system76driver/daemon.py
@@ -43,6 +43,7 @@ NEEDS_AIRPLANE = frozenset([
 
 # Products in this dict need the brightness hack
 DEFAULT_BRIGHTNESS = {
+    'galu1': ('acpi_video0', 70),
     'gazp9': ('intel_backlight', 690),
     'sabc1': ('acpi_video0', 82),
 }
