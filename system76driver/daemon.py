e5f88a6973ab2ab5d2f20b4fae75aa917249ff45
Jason Gerard DeRose <jason@system76.com>
2013-10-06 14:21:34 -0600

    set daru4 default brightness at 569

--- system76driver/daemon.py
+++ system76driver/daemon.py
@@ -48,7 +48,7 @@ NEEDS_AIRPLANE = frozenset([
 # Products in this dict need the brightness hack
 DEFAULT_BRIGHTNESS = {
     'bonx7': ('acpi_video0', 64),
-    'daru4': ('intel_backlight', 661),
+    'daru4': ('intel_backlight', 569),
     'galu1': ('acpi_video0', 70),
     'gazp9': ('intel_backlight', 690),
     'sabc1': ('acpi_video0', 82),
