0cde6861b51d59a88bc5ad40c35089e379877a11
Jason Gerard DeRose <jason@system76.com>
2013-11-15 15:10:41 -0700

    Bumped version to 13.10.5

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -24,7 +24,7 @@ Universal driver for System76 computers
 from os import path
 
 
-__version__ = '13.10.4'
+__version__ = '13.10.5'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 
