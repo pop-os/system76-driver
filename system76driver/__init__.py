6955b834fef3e631f41986a2537f8723666156ac
Jason Gerard DeRose <jason@system76.com>
2014-04-17 17:37:19 -0600

    Bump version to 14.04.2

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -24,7 +24,7 @@ Universal driver for System76 computers
 from os import path
 
 
-__version__ = '14.04.1'
+__version__ = '14.04.2'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 
