d7f54509246c4943c9c611a1c623758b62ddce73
Jason Gerard DeRose <jason@system76.com>
2015-03-18 13:41:59 -0600

    Bump version to 15.04.0, start Vivid series

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -24,7 +24,7 @@ Universal driver for System76 computers
 from os import path
 
 
-__version__ = '14.10.8'
+__version__ = '15.04.0'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 
