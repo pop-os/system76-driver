46bb733a28f2a6e7671ff1a71ce7977c8fe12a40
Jason Gerard DeRose <jason@system76.com>
2015-11-19 14:13:40 -0700

    Bump version to 15.10.6~alpha

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -24,7 +24,7 @@ Universal driver for System76 computers
 from os import path
 
 
-__version__ = '15.10.5'
+__version__ = '15.10.6'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 
