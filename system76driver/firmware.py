0caa23e62a194d7d9118fee82ae85441a3d6003c
David Jordan <david2@system76.com>
2017-07-24 12:20:26 -0600

    functional firmware dialog, version checking, and release notes.

--- system76driver/firmware.py
+++ system76driver/firmware.py
@@ -21,9 +21,7 @@
 Firmware updater for System76 computers.
 """
 
-import datetime
 from .ecflash import Ec
-from email.utils import parsedate
 import nacl.encoding
 import nacl.signing
 import nacl.hash
@@ -36,6 +34,7 @@ from os import path
 import os
 import shutil
 import subprocess
+import json
 from .mockable import SubProcess
 from gi.repository import GLib
 
@@ -43,7 +42,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
-FIRMWARE_URI = 'http://iso.system76.com/firmware/current/'
+FIRMWARE_URI = 'http://iso.system76.com/firmware/master/'
 
 FIRMWARE_SET_NEXT_BOOT = """#!/bin/bash -e
 
@@ -67,12 +66,29 @@ efibootmgr -n 1776
 echo -e "\e[1mInstalled system76-firmware-update\e[0m" >&2
 """
 
-def get_project_hash():
+def get_ec_version():
     ec = Ec()
-    project = ec.project()
+    version = ec.version()
     ec.close()
-
-    return nacl.hash.sha256(bytes(project, 'utf8'), encoder=nacl.encoding.HexEncoder).decode('utf-8')
+    return version
+    
+def get_bios_version():
+    f = open("/sys/class/dmi/id/bios_version")
+    version = f.read().strip()
+    f.close()
+    return version
+    
+def needs_update(new_bios_version, new_ec_version):
+    if not new_bios_version:
+        log.warn("Couldn't get the new bios version from changelog!")
+    elif new_bios_version != get_bios_version():
+        return True
+    if not new_ec_version:
+        log.warn("Couldn't get the new ec version from changelog!")
+    elif new_ec_version != get_ec_version():
+        return True
+    return False
+        
 
 def get_url(filename):
     if not filename:
@@ -80,16 +96,15 @@ def get_url(filename):
         model = f.read().strip()
         f.close()
 
-        project_hash = get_project_hash()
+        ec = Ec()
+        project = ec.project()
+        ec.close()
+
+        project_hash = nacl.hash.sha256(bytes(project, 'utf8'), encoder=nacl.encoding.HexEncoder).decode('utf-8')
+
         filename = "{}_{}".format(model, project_hash)
 
-    return 'http://iso.system76.com/firmware/current/{}'.format(filename)
-
-def get_info(filename=None):
-    request.urlcleanup()
-    response = request.urlopen(get_url(filename))
-    return response.info()
-
+    return 'http://iso.system76.com/firmware/master/{}'.format(filename)
 
 def get_signed_tarball(filename=None):
     request.urlcleanup()
@@ -111,6 +126,114 @@ def extract_tarball(tar, directory):
     tar.extractall(directory)
     os.chmod(directory, 0o500)
 
+def confirm_dialog(changes_list=['No Changes']):
+    user_name = subprocess.check_output(
+                    "who | awk -v vt=tty$(fgconsole) '$0 ~ vt {print $1}'",
+                    shell=True
+                ).decode('utf-8').rstrip('\n')
+
+    display_name = subprocess.check_output(
+                    "who | awk -v vt=tty$(fgconsole) '$0 ~ vt {print $5}'",
+                    shell=True
+                ).decode('utf-8').rstrip('\n').lstrip('(').rstrip(')')
+            
+    user_pid = subprocess.check_output(
+                    "who -u | awk -v vt=tty$(fgconsole) '$0 ~ vt {print $6}'",
+                    shell=True
+                ).decode('utf-8').rstrip('\n')
+            
+    user_session_pids = subprocess.check_output(['pgrep', '-P', user_pid]
+                ).decode('utf-8').rstrip('\n')
+    user_session_pid = user_session_pids.split()[0]
+            
+    environ = subprocess.check_output(['cat', '/proc/' + str(user_session_pid) 
+                                      + '/environ']
+                ).decode('utf-8').rstrip('\n')
+            
+    if "DESKTOP_SESSION=gnome" in environ:
+        desktop_env = 'gnome'
+    #changes = ["Quieter fan curve", "Added HyperThreading toggle"]
+    changes = changes_list
+    bios_changes = ["Quieter fan curve", "Added HyperThreading toggle"]
+    ec_changes = ["Quieter fan curve"]
+
+    if len(user_name) == 0 or len(display_name) == 0:
+        return
+
+    args = [
+        "sudo",
+        "DESKTOP_SESSION=" + desktop_env,
+        "FIRMWARE_CHANGES=" + json.dumps(changes),
+        "BIOS_CHANGES=" + json.dumps(bios_changes),
+        "EC_CHANGES=" + json.dumps(ec_changes),
+        "su",
+        user_name,
+        "XAUTHORITY=/home/" + user_name + "/.Xauthority", 
+        "DISPLAY=" + display_name,
+        "-c",
+        './system76-firmware-dialog',
+    ]
+
+    return subprocess.call(args)
+
+def read_changelog(f):
+    version_token = None
+    indent_level = 0
+    section = None
+    sections = []
+    for line in f.readlines():
+        #parse 'version'
+        if not version_token:
+            if line == 'versions:\n':
+                version_token = line
+                indent_level = 6
+        else:
+            #Parse each section.  Indented section starts with a '-'
+            if line[4] == '-':
+                if section:
+                    sections.append(section)
+                section = {'description': None, 'bios': None, 'ec': None, 'ec2': None}
+            if True:
+                section_line = line[indent_level:]
+                for key in ['description', 'bios', 'ec', 'ec2']:
+                    key_str = key + ': '
+                    if (section_line.startswith(key_str) and len(section_line) > len(key_str)):
+                        value = section_line[len(key_str):].strip('\n')
+                        section[key] = value
+    sections.append(section)
+    return sections
+
+def get_changes_list(changelog_entries, current_bios, current_ec, current_ec2=None):
+    found_bios = False
+    found_ec = False
+    found_ec2 = False
+    changes_list = []
+    if not current_ec2:
+        found_ec2 = True
+    if not current_ec:
+        pass
+    if not current_bios:
+        pass
+    for entry in changelog_entries:
+        if True:
+            if entry['bios'] and current_bios:
+                if current_bios >= entry['bios']:
+                    found_bios = True
+            if entry['ec'] and current_ec:
+                if current_ec >= entry['ec']:
+                    found_ec = True
+            if entry['ec2'] and current_ec2:
+                if current_ec2 >= entry['ec2']:
+                    found_ec2 = True
+        if not (found_bios and found_ec and found_ec2):
+            changes_list.append(entry['description'])
+        else:
+            break
+    if changes_list == []:
+        changes_list.append('No Changes')
+    return changes_list
+            
+
 def set_next_boot():
     handle, name = tempfile.mkstemp()
     f = open(handle, 'w')
@@ -123,85 +246,44 @@ def set_next_boot():
         return
 
 def _run_firmware_updater(model):
-    if path.isdir('/boot/efi/system76-firmware-update'):
-        old_date = path.getmtime('/boot/efi/system76-firmware-update')
-        print(old_date)
-    else:
-        old_date = 0
-
-    #Check for newer timestamp
-    info = get_info()
-    html_date = info['Last-Modified']
-    date = datetime.datetime(*parsedate(html_date)[:6]).timestamp()
-    print(date, old_date)
-
-    if old_date >= date:
-        print("Already up to date")
-        return
-
-    #Wait for user
-    while True:
-        names = subprocess.check_output(
-                "who | awk -v vt=tty$(fgconsole) '$0 ~ vt {print $1 \",\" $5}'",
-                shell=True
-        ).decode('utf-8').rstrip('\n').split(',')
-
-        if len(names) == 2:
-            user_name = names[0]
-            display_name = names[1].lstrip('(').rstrip(')')
-            log.info("User %r logged in at %r", user_name, display_name)
-            break
-        else:
-            log.info("Waiting for logged in user")
-            time.sleep(5)
-
     #Download the latest updater and firmware for this machine and verify source.
-    if not model:
-        firmware = get_signed_tarball()
-    else:
-        firmware = get_signed_tarball("{}_{}".format(model, get_project_hash()))
-        
-    if not firmware:
-        log.info("Firmware package not found")
-        return
-
     updater = get_signed_tarball('system76-firmware-update')
-    if not updater:
-        log.info("Firmware updater not found")
-        return
+    firmware = get_signed_tarball()
 
-    #Extract to temporary directory and set safe permissions.
-    with tempfile.TemporaryDirectory() as tempdirname:
-        extract_tarball(updater, tempdirname)
-        os.mkdir(path.join(tempdirname, 'firmware'))
-        extract_tarball(firmware, path.join(tempdirname, 'firmware'))
+    if updater and firmware:
+        #Extract to temporary directory and set safe permissions.
+        with tempfile.TemporaryDirectory() as tempdirname:
+            extract_tarball(updater, tempdirname)
+            os.mkdir(path.join(tempdirname, 'firmware'))
+            extract_tarball(firmware, path.join(tempdirname, 'firmware'))
+            
+            #Process changelog and component versions
+            with open(path.join(tempdirname, 'firmware', 'changelog.yaml')) as f:
+                changelog_entries = read_changelog(f)
+                
+                #Don't offer the update if its already installed
+                if not needs_update(changelog_entries[0]['bios'], changelog_entries[0]['ec']):
+                    log.info('No new firmware to install.')
+                    return
+                    
+                changes_list = get_changes_list(changelog_entries, get_bios_version(), get_ec_version())
 
-        log.info("Prompting %r at %r for firmware installation", user_name, display_name)
+            #Confirm installation with the user.
+            if confirm_dialog(changes_list) == 0:
+                log.info("Setting up firmware installation.")
 
-        #Confirm installation with the user.
-        args = [
-            "sudo",
-            "-u", user_name,
-            "DISPLAY=" + display_name,
-            "--",
-            "./system76-firmware-dialog"
-        ]
+                #Remove old firmware updater
+                try:
+                    shutil.rmtree('/boot/efi/system76-firmware-update')
+                except:
+                    pass
 
-        code = subprocess.call(args)
-        if code == 0:
-            log.info("Setting up firmware installation.")
-
-            #Remove old firmware updater
-            if path.isdir('/boot/efi/system76-firmware-update'):
-                shutil.rmtree('/boot/efi/system76-firmware-update')
-
-            #Install firmware to /efi/boot and set boot.efi on next boot.
-            shutil.copytree(tempdirname, '/boot/efi/system76-firmware-update')
-            set_next_boot()
-
-            log.info("Installed firmware updater to boot partition. Firmware update will run on next boot.")
-        else:
-            log.info("Firmware installation cancelled with code %r", code)
+                #Install firmware to /efi/boot and set boot.efi on next boot.
+                shutil.copytree(tempdirname, '/boot/efi/system76-firmware-update')
+                set_next_boot()
+            else:
+                return
+    log.info("Installed firmware updater to boot partition. Firmware update will run on next boot.")
 
 def run_firmware_updater(model):
     try:
