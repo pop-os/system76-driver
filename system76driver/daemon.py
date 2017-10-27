ce4226b7c42e06858d57d227fdfc8f3d3353fc36
David Jordan <david2@system76.com>
2017-10-27 12:38:22 -0600

    system76driver/daemon.py: limit xbacklight fix to nvidia models

--- system76driver/daemon.py
+++ system76driver/daemon.py
@@ -356,7 +356,7 @@ class Brightness:
     def set_xbacklight(self, brightness):
         xbrightness = int(100 * brightness / self.xbacklight_max_brightness)
         if xbrightness == 0:
-            xbrightness = 1:
+            xbrightness = 1
         if xbrightness <= 100:
             xbrightness_cmd = ['xbacklight', 
                                '-set', 
@@ -382,7 +382,8 @@ class Brightness:
     def run(self):
         self.xbacklight_max_brightness = self.read_max_brightness()
         self.timeout_id = GLib.timeout_add(10 * 1000, self.on_timeout)
-        self.timeout_id = GLib.timeout_add(100, self.update_xbacklight)
+        if self.model in NEEDS_BRIGHTNESS_ACPI:
+            self.timeout_id = GLib.timeout_add(100, self.update_xbacklight)
 
     def on_timeout(self):
         try:
