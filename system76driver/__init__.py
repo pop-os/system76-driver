6884b68875fc999758c414738c19d84c7aee5430
Jason Gerard DeRose <jason@system76.com>
2018-01-02 15:08:36 -0700

    Bump version to 17.10.12~alpha

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -25,7 +25,7 @@ from os import path
 import logging
 
 
-__version__ = '17.10.11'
+__version__ = '17.10.12'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 log = logging.getLogger(__name__)
