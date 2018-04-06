4ade58a14bb7caa9230fcbc0556c479ec7d9e71a
Jeremy Soller <jeremy@system76.com>
2018-04-06 09:27:02 -0600

    Bump version to 16.04.53~alpha

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -25,7 +25,7 @@ from os import path
 import logging
 
 
-__version__ = '16.04.52'
+__version__ = '16.04.53'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 log = logging.getLogger(__name__)
