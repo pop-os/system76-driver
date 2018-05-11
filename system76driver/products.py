39963846a3e56890210b9ac50141a69d11629f0d
David Jordan <david2@system76.com>
2018-05-11 16:21:12 -0600

    add nvreg_enablebacklighthandler to bonw13

--- system76driver/products.py
+++ system76driver/products.py
@@ -102,6 +102,7 @@ PRODUCTS = {
             actions.dac_fixup,
             actions.pulseaudio_hp_spdif_desc,
             actions.hidpi_scaling,
+            actions.nvreg_enablebacklighthandler,
         ],
     },
 
