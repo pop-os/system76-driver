16d3d4bf6f0e4ecee34d3131e9a538c288207eb9
Jeremy Soller <jeremy@system76.com>
2018-08-15 13:24:53 -0600

    Add cosmic release

--- bump-version.py
+++ bump-version.py
@@ -34,7 +34,7 @@ from system76driver import __version__
 from system76driver.tests.helpers import TempDir
 
 
-DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic')
+DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic')
 ALPHA = '~~alpha'
 
 TREE = path.dirname(path.abspath(__file__))
--- make-release.py
+++ make-release.py
@@ -34,7 +34,7 @@ from system76driver import __version__
 from system76driver.tests.helpers import TempDir
 
 
-DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic')
+DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic')
 PPA = 'ppa:system76-dev/pre-stable'
 ALPHA = '~~alpha'
 
