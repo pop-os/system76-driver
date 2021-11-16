4a32d0f53ac6a363bc0417be6f43b1a243559385
Jeremy Soller <jeremy@system76.com>
2021-11-16 14:41:51 -0700

    Bump version to 20.04.45~~alpha

--- system76driver/__init__.py
+++ system76driver/__init__.py
@@ -25,7 +25,7 @@ from os import path
 import logging
 
 
-__version__ = '20.04.44'
+__version__ = '20.04.45'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 log = logging.getLogger(__name__)
