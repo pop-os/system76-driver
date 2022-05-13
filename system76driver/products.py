5dea7fea30f4c14b733b92f33f6b3bb21e598208
XV-02 <daniel@system76.com>
2022-05-13 13:24:03 -0600

    Adds Thelio Mira R2

--- system76driver/products.py
+++ system76driver/products.py
@@ -1050,6 +1050,10 @@ PRODUCTS = {
         'name': 'Thelio Mira',
         'drivers': [],
     },
+    'thelio-mira-r2': {
+        'name': 'Thelio Mira',
+        'drivers': [],
+    },
 
     # Wildebeest:
     'wilb1': {
