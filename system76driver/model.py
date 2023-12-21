1dc5354555519a1360fe67f0164513f49a7d04bf
leviport <levi@system76.com>
2023-12-21 09:31:37 -0700

    Add thelio-major-r5

--- system76driver/model.py
+++ system76driver/model.py
@@ -262,6 +262,7 @@ TABLES = {
         'thelio-major-r2.1': 'thelio-major-r2.1',
         'thelio-major-r3': 'thelio-major-r3',
         'thelio-major-r4': 'thelio-major-r4',
+        'thelio-major-r5': 'thelio-major-r5',
         'thelio-massive-b1': 'thelio-massive-b1',
         'thelio-mega-b1': 'thelio-mega-b1',
         'thelio-mega-r1': 'thelio-mega-r1',
