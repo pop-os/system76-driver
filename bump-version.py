46d02003e32ed9a596d7691c46cd328250abf0f7
Jeremy Soller <jackpot51@gmail.com>
2025-02-24 11:44:07 -0700

    Add jammy to scripts

--- bump-version.py
+++ bump-version.py
@@ -34,7 +34,7 @@ from system76driver import __version__
 from system76driver.tests.helpers import TempDir
 
 
-DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan', 'focal')
+DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan', 'focal', 'jammy')
 ALPHA = '~~alpha'
 
 TREE = path.dirname(path.abspath(__file__))
